b52dca61f174314aaedc5db28a5e2bb56d279ad3
Sarah Lawrence <s.j.lawrence8@gmail.com>
2020-12-21 17:56:00 +1100

    basic side input

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,18 @@ import "./App.css";
 
 function App() {
   const [result, setResult] = useState(0);
+  const [sides, setSides] = useState(6);
+
   return (
     <div className="App">
+      <p>Sides: {`${sides}`}</p>
+      <input
+        type="number"
+        value={sides}
+        onChange={(e) => setSides(parseInt(e.target.value))}
+      />
       <p>{result ? `The current roll is ${result}` : "Click to roll!"}</p>
-      <button onClick={() => setResult(roll(6))}>ROLL</button>
+      <button onClick={() => setResult(roll(sides))}>ROLL</button>
     </div>
   );
 }
