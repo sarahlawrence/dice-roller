1401979990b55604eb793b3bf6109d6b080d33ea
Sarah Lawrence <s.j.lawrence8@gmail.com>
2020-12-21 17:42:02 +1100

    add basic click to roll

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import { roll } from "./utils/roll";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [result, setResult] = useState(0);
+  return (
+    <div className="App">
+      <p>{result ? `The current roll is ${result}` : "Click to roll!"}</p>
+      <button onClick={() => setResult(roll(6))}>ROLL</button>
+    </div>
+  );
 }
 
 export default App;
